69f18e71e7fefce1c99395ba1fb8a9e991271b90
Yahyo <hp50235@gmail.com>
2021-02-06 18:02:35 +0500

    DElete qilidim

--- admin-server/src/main/java/uz/gvs/admin_crm/controller/ClientController.java
+++ admin-server/src/main/java/uz/gvs/admin_crm/controller/ClientController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import uz.gvs.admin_crm.payload.ApiResponse;
 import uz.gvs.admin_crm.payload.ClientDto;
+import uz.gvs.admin_crm.repository.ClientRepository;
 import uz.gvs.admin_crm.service.ApiResponseService;
 import uz.gvs.admin_crm.service.ClientService;
 
@@ -18,17 +19,28 @@ public class ClientController {
     ApiResponseService apiResponseService;
     @Autowired
     ClientService clientService;
+    @Autowired
+    ClientRepository clientRepository;
 
     @PostMapping
-    public HttpEntity<?> addClient(@RequestBody ClientDto clientDto){
+    public HttpEntity<?> addClient(@RequestBody ClientDto clientDto) {
         ApiResponse apiResponse = clientService.addClient(clientDto);
-        return ResponseEntity.status(apiResponse.isSuccess() ? 200 :409).body(apiResponse);
+        return ResponseEntity.status(apiResponse.isSuccess() ? 200 : 409).body(apiResponse);
     }
 
     @PutMapping("/{id}")
-    public HttpEntity<?> editClient(@RequestBody ClientDto clientDto, @PathVariable UUID id){
+    public HttpEntity<?> editClient(@RequestBody ClientDto clientDto, @PathVariable UUID id) {
         ApiResponse apiResponse = clientService.editClient(id, clientDto);
         return ResponseEntity.status(apiResponse.isSuccess() ? 202 : 409).body(apiResponse);
+    }
 
+    @DeleteMapping("{id}")
+    public HttpEntity<?> deleteClient(@PathVariable UUID id) {
+        try {
+            clientRepository.deleteById(id);
+            return  ResponseEntity.status(204).body(apiResponseService.deleteResponse());
+        }catch (Exception a){
+            return ResponseEntity.status(409).body(apiResponseService.tryErrorResponse());
+        }
     }
 }
