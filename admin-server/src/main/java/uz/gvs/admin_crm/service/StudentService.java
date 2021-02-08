bd9b8d3b114e7f4bb4cfd0dac2952416a284728c
Abdurakhmon <yusufjonov01@gmail.com>
2021-02-08 16:57:09 +0500

    STUDENT

--- admin-server/src/main/java/uz/gvs/admin_crm/service/StudentService.java
+++ admin-server/src/main/java/uz/gvs/admin_crm/service/StudentService.java
@@ -67,7 +67,6 @@ public class StudentService {
                 if (b) {
                     return apiResponseService.existResponse();
                 }
-
                 user.setPhoneNumber(studentDto.getPhoneNumber());
                 user.setFullName(studentDto.getFullName());
                 user.setDescription(studentDto.getDescription());
