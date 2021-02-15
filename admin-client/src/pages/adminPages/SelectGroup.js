cf85e5132ebc055716cf9537b65a4a47762dceb6
Mashhurbek <mashrabov4438@gmail.com>
2021-02-15 12:01:06 +0500

    dsds

--- admin-client/src/pages/adminPages/SelectGroup.js
+++ admin-client/src/pages/adminPages/SelectGroup.js
@@ -181,15 +181,14 @@ class SelectGroup extends Component {
                                                         ...
                                                     </DropdownToggle>
                                                     <DropdownMenu>
-                                                        {/*<DropdownItem header>Header</DropdownItem>*/}
-                                                        {/*<DropdownItem>Some Action</DropdownItem>*/}
-                                                        {/*<DropdownItem text>Dropdown Item Text</DropdownItem>*/}
-                                                        {/*<DropdownItem disabled>Action (disabled)</DropdownItem>*/}
-                                                        {/*<DropdownItem divider/>*/}
-                                                        {/*<DropdownItem>Foo Action</DropdownItem>*/}
-                                                        {/*<DropdownItem>Bar Action</DropdownItem>*/}
-                                                        {/*<DropdownItem>Quo Action</DropdownItem>*/}
-                                                        <DropdownItem>Move stodent to group</DropdownItem>
+                                                        <DropdownItem header>Header</DropdownItem>
+                                                        <DropdownItem>Some Action</DropdownItem>
+                                                        <DropdownItem text>Dropdown Item Text</DropdownItem>
+                                                        <DropdownItem disabled>Action (disabled)</DropdownItem>
+                                                        <DropdownItem divider/>
+                                                        <DropdownItem>Foo Action</DropdownItem>
+                                                        <DropdownItem>Bar Action</DropdownItem>
+                                                        <DropdownItem>Quo Action</DropdownItem>
                                                     </DropdownMenu>
                                                 </Dropdown>
                                             </div>
