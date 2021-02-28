a316e06f22a6a6f41f02f17341af4159aed1ee01
Mashhurbek <mashrabov4438@gmail.com>
2021-02-28 22:08:38 +0500

    List Salary

--- admin-client/src/utils/config.js
+++ admin-client/src/utils/config.js
@@ -1,5 +1,4 @@
 export const config = {
-    BASE_URL: 'http://localhost/api',
-    // BASE_URL: 'http://185.251.88.135/api',
-    //
+    // BASE_URL: 'http://localhost/api',
+    BASE_URL: 'http://localhost:9000/api',
 };
