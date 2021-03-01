6da7dedc6e6ca026f0434b3de34ce1d83fd74b5a
Muhammadaziz Mirahmadov <1410warrior@gmail.com>
2021-03-01 11:09:06 +0500

    Muhammadaziz

---
+++ admin-client/src/pages/adminPages/Attandance.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import AdminLayout from "../../component/AdminLayout";
+
+class Attandance extends Component {
+    render() {
+        return (
+            <div>
+                <AdminLayout pathname={this.props.location.pathname}>
+
+                </AdminLayout>
+            </div>
+        );
+    }
+}
+
+Attandance.propTypes = {};
+
+export default Attandance;
