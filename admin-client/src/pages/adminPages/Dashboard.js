e2adfc0154816b419a3413dc4d526de622b9af9a
Muhammadaziz Mirahmadov <1410warrior@gmail.com>
2021-03-06 16:25:33 +0500

    Muhammadaziz

--- admin-client/src/pages/adminPages/Dashboard.js
+++ admin-client/src/pages/adminPages/Dashboard.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import AdminLayout from "../../component/AdminLayout";
 import './adminPages.scss';
