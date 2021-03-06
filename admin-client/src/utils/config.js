951c74f8de0694453546c13b8238bb05beb87648
Muhammadaziz Mirahmadov <1410warrior@gmail.com>
2021-03-06 17:24:53 +0500

    Muhammadaziz

--- admin-client/src/utils/config.js
+++ admin-client/src/utils/config.js
@@ -1,4 +1,4 @@
 export const config = {
-    // BASE_URL: 'http://localhost/api',
-    BASE_URL: 'http://localhost:9000/api',
+    BASE_URL: 'http://localhost/api',
+    // BASE_URL: 'http://localhost:9000/api',
 };
